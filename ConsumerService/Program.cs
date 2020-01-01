dc0091e362e0ef407c415e0210f39b595083e6ab
Raviteja444 <pagidojuraviteja2@gmail.com>
2020-01-01 06:06:35 +0530

    Adding all the local repo changes to remote

---
+++ ConsumerService/Program.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using MassTransit.RabbitMqTransport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunMassTransitReceiverWithRabbit();
+        }
+
+        private static void RunMassTransitReceiverWithRabbit()
+        {
+            IBusControl rabbitBusControl = Bus.Factory.CreateUsingRabbitMq(rabbit =>
+            {
+                IRabbitMqHost rabbitMqHost = rabbit.Host(new Uri("rabbitmq://localhost:5672"), settings =>
+                {
+                    settings.Password("guest");
+                    settings.Username("guest");
+                });
+
+                rabbit.ReceiveEndpoint(rabbitMqHost, "mycompany.domains.queues", conf =>
+                {
+                    conf.Consumer<RegisterCustomerConsumer>();
+                });
+            });
+
+            rabbitBusControl.Start();
+            Console.ReadKey();
+
+            rabbitBusControl.Stop();
+        }
+    }
+}
---
+++ ConsumerService/RegisterCustomerConsumer.cs
@@ -0,0 +1,48 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCompany.Messaging;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsumerService
+{
+    public class RegisterCustomerConsumer : IConsumer<IRegisterCustomer>
+    {
+        public Task Consume(ConsumeContext<IRegisterCustomer> context)
+        {
+            IRegisterCustomer newCustomer = context.Message;
+
+            var fromAddress = new MailAddress("", "");
+            var toAddress = new MailAddress("", "");
+            const string fromPassword = "";
+            const string subject = "Test";
+            string body = "Hie this is a test message"+newCustomer.Name;
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                EnableSsl = true
+            };
+            
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+
+            return Task.FromResult(context.Message);
+        }
+    }
+}
